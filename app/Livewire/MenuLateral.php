5c9d7868cf49553cdc18642296807ecef2b255e9
VictoriaVMC <94030658+victoriavmc@users.noreply.github.com>
2025-02-05 21:42:41 -0300

    Layaout Sidebar

---
+++ app/Livewire/MenuLateral.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class MenuLateral extends Component
+{
+    public function render()
+    {
+        return view('livewire.menu-lateral');
+    }
+}
