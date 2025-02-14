4e6d845f646bdf3c9780127cbba4ad1be8483c4c
VictoriaVMC <94030658+victoriavmc@users.noreply.github.com>
2025-02-14 01:03:02 -0300

    Forms Create Person and Employer

--- app/Livewire/Usuarios/Trabajadores.php
+++ app/Livewire/Usuarios/Trabajadores.php
@@ -2,10 +2,33 @@
 
 namespace App\Livewire\Usuarios;
 
+use App\Models\Jobposition;
 use Livewire\Component;
 
 class Trabajadores extends Component
 {
+    // Variable para almacenar los trabajadores
+    public $trabajadores;
+
+    // Variable parusuarios.createa almacenar el trabajador seleccionado
+    public $trabajador;
+
+    //Datos necesarios para la paginación
+    public $idJobposition; // id del trabajador
+    public $profile_photo_path; // Foto de perfil
+    public $firstName; //Nombre Completo
+    public $lastName; //Apellido Completo
+    public $position; //Nombre deL area que cubre
+    public $startDate; //Nombre del inicio que comenzo a trabajar
+    public $status; //Estado del trabajador
+
+
+    public function mount()
+    {
+        // Cargamos los datos necesarios, de todos los trabajadores
+        $this->trabajadores = Jobposition::all();
+    }
+
     public function render()
     {
         return view('livewire.usuarios.trabajadores');
--- resources/views/livewire/usuarios/trabajadores.blade.php
+++ resources/views/livewire/usuarios/trabajadores.blade.php
@@ -1,3 +1,44 @@
 <div>
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <div class="w-full py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden shadow-xl bg-base-300 sm:rounded-lg">
+                <div class="overflow-x-auto">
+                    <table class="table">
+                        <!-- head -->
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Perfil</th>
+                                <th>Nombre Completo</th>
+                                <th>Posición</th>
+                                <th>Fecha de ingreso</th>
+                                <th>Estado</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- row 1 -->
+                            {{-- Aqui paso los datos del amount trabajadores --}}
+                            @foreach ($trabajadores as $trabajador)
+                                <tr>
+                                    <th>{{ $trabajador->idJobPosition }}</th>
+                                    <td>{{ $trabajador->person->users->profile_photo_path ?? 'No' }}</td>
+                                    <td>{{ $trabajador->person->personaldata->firstName . ' ' . $trabajador->person->personaldata->lastName }}
+                                    </td>
+                                    <td>{{ $trabajador->position }}</td>
+                                    <td>{{ $trabajador->startDate }}</td>
+                                    <td>{{ $trabajador->status }}</td>
+                                    <td>
+                                        Ver todos los datos. Modicar datos personales. Modificar datos laborales.
+                                        Eliminarlo.
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
